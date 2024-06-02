5e014f8e95e6efba72e74aa8e9c3b129349e244a
Mihyeon <kim20170@naver.com>
2024-06-02 23:33:41 +0900

    TEST 코드 추가

--- team4.ino
+++ team4.ino
@@ -8,10 +8,24 @@ int btn = 8;
 void setup()
 {
     lcd.begin(16,2);
+    pinMode(10,OUTPUT);
 }
 
 void loop()
 {
-    lcd.setCursor(0,1);
-    lcd.print("Hello World!");
+    // LCD TEST
+    // lcd.setCursor(0,1);
+    // lcd.print("Hello World!");
+
+    // 피에조 TEST
+    // analogWrite(10,64);
+    // analogWrite(10,0);
+    
+    // 초음파 TEST
+    // if (duration==0) return;
+    // long distnce=duration / 58.2;
+    // if (distnce<25){
+    //     lcd.setCursor(0,1);
+    //     lcd.print("Hello World!");
+    // }
 }
